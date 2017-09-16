d33cf2eec001ffd3d61c9bfe0f2923b046a08fed
Moritz Meier <moritzmeier@aol.com>
2017-09-16 22:13:35 +0200

    Shell support.

--- treepattern.py
+++ treepattern.py
@@ -1,33 +1,67 @@
-from builder import GraphBuilder
-from matcher import PatternMatcher
 import logging
-
+import argparse
+import os
 
 def parse_args():
-    import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("tree", help="Stanford parser output.")
+    parser.add_argument("--debug", action="store_true", help="Set logging level to 'DEBUG'.")
+    parser.add_argument("--info", action="store_true", help="Set logging level to 'INFO'.")
+    parser.add_argument("rules", help="File with rules.")
+    parser.add_argument("sents", help="English sentences.")
     return parser.parse_args()
 
 
 def main():
-    logging.basicConfig(level=logging.INFO)
-
-    with open('rules') as f:
+    args = parse_args()
+    
+    
+    if args.debug:
+        level = logging.DEBUG
+    elif args.info:
+        level = logging.INFO
+    else:
+        level = logging.WARNING
+    logging.basicConfig(level=level)
+    
+    
+    # set Stanford parser and model path
+    parser_path = os.environ['STANFORD_PARSER']
+    parser_path = os.path.expanduser(parser_path)
+    os.environ['STANFORD_PARSER'] = parser_path
+        
+    if 'STANFORD_MODELS' in os.environ:
+        models_path = os.environ['STANFORD_MODELS']
+        models_path = os.path.expanduser(models_path)
+        os.environ['STANFORD_MODELS'] = models_path
+    else:
+        os.environ['STANFORD_MODELS'] = os.environ['STANFORD_PARSER']
+       
+    
+    with open(args.rules) as f:
         rules = f.read()
+ 
 
+    from matcher import PatternMatcher 
     matcher = PatternMatcher.from_str(rules)
+    
+    from builder import GraphBuilder
     builder = GraphBuilder(matcher)
-
-    for graph in builder.build(sents):
+    
+    for graph in builder.build(args.sents):
         dot_code = graph.to_dot()
         print(dot_code)
 
+        
+if __name__ == '__main__':
+    main()
+  
 
-from parse import parse_tree_str
+"""from parse import parse_tree_str
 tree = parse_args().tree
-tree = parse_tree_str(tee)
-    
+tree = parse_tree_str(tree)
+
+print(tree)"""
+
 
 """print()
 print()
